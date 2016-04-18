7037142f7f9f5c3817d9a94c95f9a4450bf64ece
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-04-18 18:08:22 -0300

    adicionado método de descoberta de bombas

--- augusteiner-poo-minesweeper/src/main/java/br/eng/augusteiner/poo/minesweeper/App.java
+++ augusteiner-poo-minesweeper/src/main/java/br/eng/augusteiner/poo/minesweeper/App.java
@@ -1,58 +1,102 @@
+
 package br.eng.augusteiner.poo.minesweeper;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * @author José Nascimento <joseaugustodearaujonascimento@gmail.com>
  */
 public class App {
+    // private static PrintStream out = System.out;
+
     public static void main(String[] args) {
 
-        char[][] field = initField(5, 5);
-        boolean gameOver = false;
+        char[][] campo = null;
 
         PrintStream out = System.out;
         Scanner scn = new Scanner(System.in);
 
-        while (!gameOver) {
-            printField(out, field);
+        campo = iniciarCampo(9, 8);
+        adicionarBombasPseudoAleatorias(campo);
+        numerarBombasProximas(campo);
 
-            scn.next();
-        }
+        imprimirCampo(out, campo);
     }
 
-    private static void printField(PrintStream out, char[][] fields) {
+    private static void numerarBombasProximas(char[][] field) {
 
-        for (int i = 0; i < fields.length; i++) {
+        for (int i = 0; i < field.length; i++) {
 
-            for (int j = 0; j < fields[0].length; j++) {
+            for (int j = 0; j < field[i].length; j++) {
 
-                out.print(fields[i][j] + "  ");
+                // XXX se tem uma bomba
+                if (field[i][j] == '*') {
+
+                    // XXX scaneando a matriz 3x3 ao redor da bomba
+                    for (int z = max(i - 1, 0); z < min(i + 2, field.length); z++) {
+
+                        for (int w = max(j - 1, 0); w < min(j + 2, field[i].length); w++) {
+
+                            if (field[z][w] == '*') {
+
+                            } else {
+                                field[z][w] += 1;
+                            }
+
+                            // out.print(String.format("[%s, %s]", z, w));
+                        }
+                    }
+
+                    // out.println();
+                }
+            }
+        }
+
+        // out.println();
+    }
+
+    private static void adicionarBombasPseudoAleatorias(char[][] campo) {
+
+        campo[2][3] = '*';
+        campo[3][4] = '*';
+        campo[4][3] = '*';
+        campo[5][5] = '*';
+        campo[6][2] = '*';
+    }
+
+    private static void imprimirCampo(PrintStream out, char[][] campo) {
+
+        char charOut;
+
+        for (int i = 0; i < campo.length; i++) {
+
+            for (int j = 0; j < campo[i].length; j++) {
+
+                charOut = campo[i][j] == '0' ? '.' : campo[i][j];
+
+                out.print(charOut + "  ");
             }
 
             out.println();
         }
     }
 
-    private static char[][] initField(int rows, int cols) {
+    private static char[][] iniciarCampo(int linhas, int colunas) {
 
-        return initField(rows, cols, rows + cols);
-    }
+        char campo[][] = new char[linhas][colunas];
 
-    private static char[][] initField(int rows, int cols, int bombs) {
+        for (int i = 0; i < linhas; i++) {
 
-        char field[][] = new char[rows][cols];
+            for (int j = 0; j < colunas; j++) {
 
-        for (int i = 0; i < rows; i++) {
-
-            for (int j = 0; j < cols; j++) {
-
-                field[i][j] = '*';
+                campo[i][j] = '0';
             }
         }
 
-        return field;
+        return campo;
     }
 }
