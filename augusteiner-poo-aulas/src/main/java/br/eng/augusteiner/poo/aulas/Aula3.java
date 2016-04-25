94247eb38b64390166c5db7cccaa4e0d62805e7e
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-04-25 16:22:25 -0300

    melhorado teste de pontos e triângulo

--- augusteiner-poo-aulas/src/main/java/br/eng/augusteiner/poo/aulas/Aula3.java
+++ augusteiner-poo-aulas/src/main/java/br/eng/augusteiner/poo/aulas/Aula3.java
@@ -8,16 +8,18 @@ public class Aula3 {
 
     public static void main(String[] args) {
 
-        Ponto p1 = new Ponto(0, 4);
+        Ponto p1 = new Ponto(0, 3);
         Ponto p2 = new Ponto(4, 0);
-        Triangulo t1 = new Triangulo(p1, p2, Ponto.ORIGEM);
 
         System.out.println(String.format("Distancia do ponto %s a origem: %s", p1, p1.distanciaOrigem()));
-
         System.out.println(String.format("Distancia do ponto %s ao ponto %s: %s", p1, p2, p1.distancia(p2)));
 
-        System.out.println(String.format("Perímetro do triângulo %s é: %s", t1, t1.perimetro()));
+        Triangulo t1 = new Triangulo(p1, p2, Ponto.ORIGEM);
 
-        System.out.println(String.format("Area do triângulo %s é: %s", t1, t1.area()));
+        System.out.println("---------------------");
+
+        System.out.println(String.format("Fatos do triângulo %s:", t1));
+        System.out.println(String.format("Perímetro do triângulo é: %s", t1.perimetro()));
+        System.out.println(String.format("Area do triângulo é: %s", t1.area()));
     }
 }
