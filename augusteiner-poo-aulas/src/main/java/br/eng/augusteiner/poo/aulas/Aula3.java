4971679da61d57e0bc66579f3546dd4a8af9b302
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-04-25 16:03:01 -0300

    adicionados output para testes da classe triângulo

--- augusteiner-poo-aulas/src/main/java/br/eng/augusteiner/poo/aulas/Aula3.java
+++ augusteiner-poo-aulas/src/main/java/br/eng/augusteiner/poo/aulas/Aula3.java
@@ -8,8 +8,8 @@ public class Aula3 {
 
     public static void main(String[] args) {
 
-        Ponto p1 = new Ponto(3, 4);
-        Ponto p2 = new Ponto(4, 2);
+        Ponto p1 = new Ponto(0, 4);
+        Ponto p2 = new Ponto(4, 0);
         Triangulo t1 = new Triangulo(p1, p2, Ponto.ORIGEM);
 
         System.out.println(String.format("Distancia do ponto %s a origem: %s", p1, p1.distanciaOrigem()));
@@ -17,5 +17,7 @@ public class Aula3 {
         System.out.println(String.format("Distancia do ponto %s ao ponto %s: %s", p1, p2, p1.distancia(p2)));
 
         System.out.println(String.format("Perímetro do triângulo %s é: %s", t1, t1.perimetro()));
+
+        System.out.println(String.format("Area do triângulo %s é: %s", t1, t1.area()));
     }
 }
