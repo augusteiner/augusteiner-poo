4aabe0c8686f14ef3bc66b28d64ee800f025feb7
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-04-28 21:06:16 -0300

    adicionadas abstrações de lâmpadas

---
+++ augusteiner-poo-lista2/src/main/java/br/eng/augusteiner/poo/lista2/Lampada.java
@@ -0,0 +1,85 @@
+
+package br.eng.augusteiner.poo.lista2;
+
+/**
+ * @author José Nascimento <joseaugustodearaujonascimento@gmail.com>
+ */
+public class Lampada {
+
+    public static final int ESTADO_APAGADA = 0;
+    public static final int ESTADO_ACESA = 1;
+    public static final int ESTADO_MEIA_LUZ = 2;
+
+    private int estado;
+
+    public Lampada() {
+
+        this.estado = ESTADO_APAGADA;
+    }
+
+    public int getEstado() {
+
+        return estado;
+    }
+
+    private void setEstado(int estado) {
+
+        this.estado = estado;
+    }
+
+    public void acender() {
+
+        setEstado(ESTADO_ACESA);
+    }
+
+    public void apagar() {
+
+        setEstado(ESTADO_APAGADA);
+    }
+
+    public void meiaLuz() {
+
+        setEstado(ESTADO_MEIA_LUZ);
+    }
+
+    public boolean estaAcesa() {
+
+        return getEstado() == ESTADO_ACESA;
+    }
+
+    public boolean estaApagada() {
+
+        return getEstado() == ESTADO_APAGADA;
+    }
+
+    public boolean estaAMeiaLuz() {
+
+        return getEstado() == ESTADO_MEIA_LUZ;
+    }
+
+    public String getEstadoComoString() {
+
+        switch (getEstado()) {
+            case ESTADO_ACESA:
+
+                return "acesa";
+            case ESTADO_APAGADA:
+
+                return "apagada";
+            case ESTADO_MEIA_LUZ:
+
+                return "meia luz";
+            default:
+
+                return "<< desconhecido >>";
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format(
+            "%s",
+            getEstadoComoString());
+    }
+}
---
+++ augusteiner-poo-lista2/src/main/java/br/eng/augusteiner/poo/lista2/LampadaContador.java
@@ -0,0 +1,88 @@
+
+package br.eng.augusteiner.poo.lista2;
+
+/**
+ * @author José Nascimento <joseaugustodearaujonascimento@gmail.com>
+ */
+public class LampadaContador extends Lampada {
+
+    private Contador contadorAcesa;
+    private Contador contadorApagada;
+    private Contador contadorMeiaLuz;
+
+    public LampadaContador() {
+
+        this(
+            Contador.novo(),
+            Contador.novo(),
+            Contador.novo());
+    }
+
+    public LampadaContador(
+        Contador contadorAcesa,
+        Contador contadorApagada,
+        Contador contadorMeiaLuz) {
+
+        this.contadorAcesa = contadorAcesa;
+        this.contadorApagada = contadorApagada;
+        this.contadorMeiaLuz = contadorMeiaLuz;
+    }
+
+    public Contador getContadorAcesa() {
+
+        return contadorAcesa;
+    }
+
+    public Contador getContadorApagada() {
+
+        return contadorApagada;
+    }
+
+    public Contador getContadorMeiaLuz() {
+
+        return contadorMeiaLuz;
+    }
+
+    @Override
+    public void acender() {
+
+        if (!estaAcesa()) {
+
+            getContadorAcesa().incrementar();
+        }
+
+        super.acender();
+    }
+
+    @Override
+    public void apagar() {
+
+        if (!estaApagada()) {
+
+            getContadorApagada().incrementar();
+        }
+
+        super.apagar();
+    }
+
+    @Override
+    public void meiaLuz() {
+
+        if (!estaAMeiaLuz()) {
+
+            getContadorMeiaLuz().incrementar();
+        }
+
+        super.meiaLuz();
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format(
+            "(L: %s, D: %s, M: %s)",
+            getContadorAcesa(),
+            getContadorApagada(),
+            getContadorMeiaLuz());
+    }
+}
