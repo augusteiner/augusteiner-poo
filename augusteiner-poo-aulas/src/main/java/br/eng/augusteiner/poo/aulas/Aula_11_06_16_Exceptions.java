e22eac4df15424249d272feb3162bef3601f8829
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-06-13 13:13:09 -0300

    adicionada aula sobre exceptions

---
+++ augusteiner-poo-aulas/src/main/java/br/eng/augusteiner/poo/aulas/Aula_11_06_16_Exceptions.java
@@ -0,0 +1,55 @@
+
+package br.eng.augusteiner.poo.aulas;
+
+import java.util.Scanner;
+
+/**
+ * @author José Nascimento <joseaugustodearaujonascimento@gmail.com>
+ */
+public class Aula_11_06_16_Exceptions {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = 0;
+
+        Integer n = null;
+        String line = null;
+
+        while (n == null) {
+
+            System.out.print("Informe um número: ");
+
+            line = sc.nextLine();
+
+            try {
+
+                n = Integer.parseInt(line);
+
+                System.out.println("Número lido com sucesso!");
+
+                System.out.println(1 / 0);
+
+            } catch (ArithmeticException e) {
+
+                System.out.println(e.getMessage());
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("Informe um número válido!");
+
+                System.out.println(e.getMessage());
+
+            } finally {
+
+                System.out.println(String.format(
+                    "Tentativa # %d",
+                    i++));
+            }
+
+        }
+
+        sc.close();
+    }
+}
