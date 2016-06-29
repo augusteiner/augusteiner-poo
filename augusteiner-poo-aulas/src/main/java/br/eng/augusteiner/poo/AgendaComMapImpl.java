1b5222c2f0c174b3195788c83248259939445b91
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-06-29 13:50:40 -0300

    adicionada aula sobre collections
    implementadas Agenda com as collections ArrayList & Hashtable

---
+++ augusteiner-poo-aulas/src/main/java/br/eng/augusteiner/poo/AgendaComMapImpl.java
@@ -0,0 +1,56 @@
+
+package br.eng.augusteiner.poo;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * @author José Nascimento <joseaugustodearaujonascimento@gmail.com>
+ */
+public class AgendaComMapImpl implements IAgenda {
+
+    private Map<String, IContato> contatos;
+
+    public AgendaComMapImpl() {
+
+        this.contatos = new Hashtable<String, IContato>();
+    }
+
+    @Override
+    public IContato buscar(String nome) throws ContatoNaoEncontradoException {
+
+        if (this.contatos.containsKey(nome)) {
+
+            return this.contatos.get(nome);
+        } else {
+
+            throw new ContatoNaoEncontradoException(nome);
+        }
+    }
+
+    @Override
+    public Iterable<IContato> getContatos() {
+
+        return this.contatos.values();
+    }
+
+    @Override
+    public void inserir(IContato contato) {
+
+        this.contatos.put(contato.getNome(), contato);
+    }
+
+    @Override
+    public int quantidadeContatos() {
+
+        return this.contatos.values().size();
+    }
+
+    @Override
+    public void remover(String nome) throws ContatoNaoEncontradoException {
+
+        IContato contato = this.buscar(nome);
+
+        this.contatos.remove(contato.getNome());
+    }
+}
