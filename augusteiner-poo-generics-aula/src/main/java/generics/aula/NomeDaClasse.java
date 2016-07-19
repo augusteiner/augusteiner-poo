f86e8a38c4c30b36bce2c07f32deb56a56a9078f
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-07-19 07:18:03 -0300

    adicionado exemplo de sintaxe de generics

--- augusteiner-poo-generics-aula/src/main/java/generics/aula/NomeDaClasse.java
+++ augusteiner-poo-generics-aula/src/main/java/generics/aula/NomeDaClasse.java
@@ -1,34 +1,20 @@
 
 package generics.aula;
 
+import generics.aula.basico.Carro;
+
 public class NomeDaClasse<T1, T2> {
 
-    private T1 membro1;
-    private T2 membro2;
+    public static <A1, A2> void nomeDoMetodo(A1 arg1, A2 arg2) {
 
-    private NomeDaClasse(T1 arg1, T2 arg2) {
-
-        this.membro1 = arg1;
-        this.membro2 = arg2;
     }
 
-    public T1 getMembro1() {
+    public static void testeMetodoGenerico() {
 
-        return this.membro1;
-    }
+        // XXX O Java sabe o tipo dos argumentos
+        nomeDoMetodo(1, 2);
 
-    public void setMembro1(T1 membro1) {
-
-        this.membro1 = membro1;
-    }
-
-    public T2 getMembro2() {
-
-        return this.membro2;
-    }
-
-    public void setMembro2(T2 membro2) {
-
-        this.membro2 = membro2;
+        // XXX Mesmo método, outros tipos de argumentos
+        nomeDoMetodo(new Carro(), 2.0);
     }
 }
